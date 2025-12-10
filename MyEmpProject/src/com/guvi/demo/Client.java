8f70bda9d443c6e1bde645d4bf0a770f418a8f45
govind <govind.corp.trainer@gmail.com>
2025-12-10 15:44:43 +0530

    removed unwanted lines

--- MyEmpProject/src/com/guvi/demo/Client.java
+++ MyEmpProject/src/com/guvi/demo/Client.java
@@ -5,10 +5,7 @@ public class Client {
 	public static void main(String[] args) {
 		System.out.println("line 1");
         System.out.println("line2");
-        System.out.println("line3");
-        System.out.println("line4");
-        System.out.println("line5");
-
+      
 	}
 
 }
